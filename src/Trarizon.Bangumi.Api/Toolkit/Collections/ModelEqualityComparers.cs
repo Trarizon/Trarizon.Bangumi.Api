e064ab6e508fd0909c4c2bc7fd720dd2a3aa98f4
Trarizon <44918595+Trarizon@users.noreply.github.com>
2025-09-01 23:57:29 +0800

    Provider id comparer and username comparer

--- src/Trarizon.Bangumi.Api/Toolkit/Collections/ModelEqualityComparers.cs
+++ src/Trarizon.Bangumi.Api/Toolkit/Collections/ModelEqualityComparers.cs
@@ -10,11 +10,64 @@ public static class ModelEqualityComparers
     /// <summary>
     /// 通过Id进行比较
     /// </summary>
-    public static IEqualityComparer<ICharacter> Id { get; }
+    public static IdComparer Id => IdComparer.Instance;
 
-    private sealed class IdComparer : IEqualityComparer<ICharacter>
+    /// <summary>
+    /// 通过UserName进行比较
+    /// </summary>
+    public static IEqualityComparer<IUserNamed> UserName => UserNameComparer.Instance;
+
+    /// <summary>
+    /// 通过Id进行比较
+    /// </summary>
+    public sealed class IdComparer
+        : IEqualityComparer<ICharacterIdentity>
+        , IEqualityComparer<IEpisodeIdentity>
+        , IEqualityComparer<IIndexIdentity>
+        , IEqualityComparer<IPersonIdentity>
+        , IEqualityComparer<IRevisionIdentity>
+        , IEqualityComparer<ISubjectIdentity>
+        , IEqualityComparer<IUserIdentity>
     {
-        public bool Equals(ICharacter? x, ICharacter? y) => x?.Id == y?.Id;
-        public int GetHashCode([DisallowNull] ICharacter obj) => obj.Id.GetHashCode();
+        internal static IdComparer Instance { get; } = new IdComparer();
+
+        private IdComparer() { }
+
+        /// <inheritdoc/>
+        public bool Equals(ICharacterIdentity? x, ICharacterIdentity? y) => x?.Id == y?.Id;
+        /// <inheritdoc/>
+        public int GetHashCode([DisallowNull] ICharacterIdentity obj) => obj.Id.GetHashCode();
+        /// <inheritdoc/>
+        public bool Equals(IEpisodeIdentity? x, IEpisodeIdentity? y) => x?.Id == y?.Id;
+        /// <inheritdoc/>
+        public int GetHashCode([DisallowNull] IEpisodeIdentity obj) => obj.Id.GetHashCode();
+        /// <inheritdoc/>
+        public bool Equals(IIndexIdentity? x, IIndexIdentity? y) => x?.Id == y?.Id;
+        /// <inheritdoc/>
+        public int GetHashCode([DisallowNull] IIndexIdentity obj) => obj.Id.GetHashCode();
+        /// <inheritdoc/>
+        public bool Equals(IPersonIdentity? x, IPersonIdentity? y) => x?.Id == y?.Id;
+        /// <inheritdoc/>
+        public int GetHashCode([DisallowNull] IPersonIdentity obj) => obj.Id.GetHashCode();
+        /// <inheritdoc/>
+        public bool Equals(IRevisionIdentity? x, IRevisionIdentity? y) => x?.Id == y?.Id;
+        /// <inheritdoc/>
+        public int GetHashCode([DisallowNull] IRevisionIdentity obj) => obj.Id.GetHashCode();
+        /// <inheritdoc/>
+        public bool Equals(ISubjectIdentity? x, ISubjectIdentity? y) => x?.Id == y?.Id;
+        /// <inheritdoc/>
+        public int GetHashCode([DisallowNull] ISubjectIdentity obj) => obj.Id.GetHashCode();
+        /// <inheritdoc/>
+        public bool Equals(IUserIdentity? x, IUserIdentity? y) => x?.Id == y?.Id;
+        /// <inheritdoc/>
+        public int GetHashCode([DisallowNull] IUserIdentity obj) => obj.Id.GetHashCode();
+    }
+
+    private sealed class UserNameComparer : IEqualityComparer<IUserNamed>
+    {
+        public static UserNameComparer Instance { get; } = new UserNameComparer();
+
+        public bool Equals(IUserNamed? x, IUserNamed? y) => x?.UserName == y?.UserName;
+        public int GetHashCode([DisallowNull] IUserNamed obj) => obj.UserName.GetHashCode();
     }
 }
